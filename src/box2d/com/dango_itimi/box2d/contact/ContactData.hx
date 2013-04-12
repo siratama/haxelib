d2931867416865a9458c546c63ff883895a608a8
siratama <webmaster@dango-itimi.com>
2013-04-12 17:36:07 +0900

    Box or Circle 接地判定処理を分離

---
+++ src/box2d/com/dango_itimi/box2d/contact/ContactData.hx
@@ -0,0 +1,39 @@
+package com.dango_itimi.box2d.contact;
+import box2D.dynamics.contacts.B2Contact;
+class ContactData {
+
+	public var b2Contact(default, null):B2Contact;
+	private var baseViewIsFixtureA:Bool;
+
+	public function new(){
+
+		initialize();
+	}
+	public function initialize(){
+		b2Contact = null;
+		baseViewIsFixtureA = false;
+	}
+	public function setData(b2Contact:B2Contact, baseViewIsFixtureA:Bool){
+
+		this.b2Contact = b2Contact;
+		this.baseViewIsFixtureA = baseViewIsFixtureA;
+	}
+	public function isContacted():Bool{
+		return b2Contact != null;
+	}
+
+	//(box on box) or (box on circle)
+	//require both angle is zero
+	public function isOnBoxOrCircle():Bool{
+
+		var manifold = b2Contact.getManifold();
+		if(manifold.m_type == 2){
+
+			return (
+				(baseViewIsFixtureA && manifold.m_localPlaneNormal.y > 0) ||
+				(!baseViewIsFixtureA && manifold.m_localPlaneNormal.y < 0)
+			);
+		}
+		return false;
+	}
+}
--- src/box2d/com/dango_itimi/box2d/contact/ContactParser.hx
+++ src/box2d/com/dango_itimi/box2d/contact/ContactParser.hx
@@ -6,65 +6,58 @@ import box2D.dynamics.contacts.B2Contact;
 class ContactParser {
 
 	private var viewKey:String;
-	private var contactCheckViewKeyMap:Hash<ContactData>;
-	public var contactedSomething(default, null):Bool;
-	public var gettedOnTheSomethingBox(default, null):Bool;
+	private var targetContactDataMap:Hash<ContactData>;
+	public var allContactData(default, null):Array<ContactData>;
 
 	public function new(viewKey:String){
 
 		this.viewKey = viewKey;
-		contactCheckViewKeyMap = new Hash();
+		targetContactDataMap = new Hash();
+		allContactData = [];
 	}
 	public function registerTargetView(targetViewKey:String){
 
-		contactCheckViewKeyMap.set(targetViewKey, new ContactData());
+		targetContactDataMap.set(targetViewKey, new ContactData());
 	}
 	public function run(b2Contact:B2Contact){
 
-		contactedSomething = false;
-		gettedOnTheSomethingBox = false;
-		for(key in contactCheckViewKeyMap.keys())
-			contactCheckViewKeyMap.get(key).initialize();
+		allContactData = [];
+		for(key in targetContactDataMap.keys())
+			targetContactDataMap.get(key).initialize();
 
 		var manifold = b2Contact.getManifold();
 		if(manifold.m_pointCount == 0) return;
 
 		var userDataA:UserData = b2Contact.getFixtureA().getBody().getUserData();
 		var userDataB:UserData = b2Contact.getFixtureB().getBody().getUserData();
-
 		var keyA:String = (userDataA != null) ? userDataA.key : "";
 		var keyB:String = (userDataB != null) ? userDataB.key : "";
 
 		if(keyA != viewKey && keyB != viewKey) return;
 
-		contactedSomething = true;
-		var gettedOnTheBox:Bool =
-			(keyA == viewKey && manifold.m_localPlaneNormal.y > 0) ||
-			(keyB == viewKey && manifold.m_localPlaneNormal.y < 0);
+		var baseViewIsFixtureA:Bool = (keyA == viewKey);
 
-		if(gettedOnTheBox) gettedOnTheSomethingBox = true;
+		var contactData = new ContactData();
+		contactData.setData(b2Contact, baseViewIsFixtureA);
+		allContactData.push(contactData);
 
-		var contactCheckViewKey:String = (keyA == viewKey) ? keyB: keyA;
-		var contactData:ContactData = contactCheckViewKeyMap.get(contactCheckViewKey);
-		if(contactData == null) return;
-
-		contactData.contacted = true;
-		if(gettedOnTheBox) contactData.gettedOnTheBox = true;
+		var contactCheckViewKey:String = (baseViewIsFixtureA) ? keyB: keyA;
+		var contactData:ContactData = targetContactDataMap.get(contactCheckViewKey);
+		if(contactData != null)
+			contactData.setData(b2Contact, baseViewIsFixtureA);
+	}
+	public function isContactedSomething():Bool{
+		return allContactData.length > 0;
 	}
 	public function getContactData(contactCheckViewKey:String):ContactData{
-		return contactCheckViewKeyMap.get(contactCheckViewKey);
+		return targetContactDataMap.get(contactCheckViewKey);
+	}
+	public function isOnSomethingBoxOrCircle():Bool{
+
+		for(contactData in allContactData){
+			if(contactData.isOnBoxOrCircle()) return true;
+		}
+		return false;
 	}
 }
 
-class ContactData{
-
-	public var contacted:Bool;
-	public var gettedOnTheBox:Bool;
-
-	public function new(){
-	}
-	public function initialize(){
-		contacted = false;
-		gettedOnTheBox = false;
-	}
-}

a0d1fb5dc2c9607696bf3522596f876546ca8d37
siratama <webmaster@dango-itimi.com>
2014-02-22 21:15:22 +0900

    added CSInterface extern for Flash CC Extension

--- src/adobe_extension/flash_extension/csinterface/CSInterface.hx
+++ src/adobe_extension/flash_extension/csinterface/CSInterface.hx
@@ -3,6 +3,9 @@ package flash_extension.csinterface;
 @:native("CSInterface")
 extern class CSInterface {
 	public function new():Void;
-	public function evalScript(script:String, ?callback:Void->Void):Void;
 	public function getSystemPath(pathType:String):String;
+
+	//script type is not String
+	//example: String + static Function
+	public function evalScript(script:Dynamic, ?callback:Dynamic->Void):Void;
 }
--- src/adobe_extension/flash_extension/csinterface/CSInterfaceUtil.hx
+++ src/adobe_extension/flash_extension/csinterface/CSInterfaceUtil.hx
@@ -1,6 +1,8 @@
 package flash_extension.csinterface;
+import jsfl.EventType;
+import jsfl.PersistentDataType;
 class CSInterfaceUtil {
-	private var csInterface:CSInterface;
+	public var csInterface(default, null):CSInterface;
 
 	public static function create():CSInterfaceUtil{
 		return new CSInterfaceUtil(new CSInterface());
@@ -16,5 +18,20 @@ class CSInterfaceUtil {
 	}
 	public function getExtensionUri():String{
 		return "file:///" + csInterface.getSystemPath(SystemPath.EXTENSION);
+	}
+	public function evalScript(script:Dynamic, ?callback:Dynamic->Void){
+		csInterface.evalScript(script, callback);
+	}
+	public function addDataToDocument(key:String, dataType:PersistentDataType, data:String){
+
+		csInterface.evalScript('document.addDataToDocument("$key", "$dataType", "$data");');
+	}
+	public function getDataFromDocument(key:String, callback:Dynamic->Void){
+		csInterface.evalScript('document.getDataFromDocument("$key");', callback);
+	}
+
+	public function addEventListener(eventType:EventType, callbackCode:String, ?callback:Dynamic->Void){
+		csInterface.evalScript(
+			'fl.addEventListener("$eventType", function(){ $callbackCode });', callback);
 	}
 }
